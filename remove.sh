515c621a34740738e12c26b13eba4adab2586cdf
kaley-neufeld <120682546+kaley-neufeld@users.noreply.github.com>
2024-11-19 13:45:43 -0500

    added error conditions

--- remove.sh
+++ remove.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
 args=("$@")
-
-echo "file(s) to remove: ${args[@]}"
-
 recycle_bin="$HOME/deleted"
 
-if [ -d "$recycle_bin" ]; then
+if [ -d "$recycle_bin" ];
+then
 	echo "deleted directory does exist"
 else
 	echo "deleted directory does NOT exist"
 	echo "creating directory"
 	mkdir $recycle_bin
 fi
+
+if [ ${#args} == 0 ];
+then
+       echo "no filename provided"
+       exit 1
+fi
+
+for file in ${args[@]}
+do
+	echo $file
+	if [ -d $file ]
+	then
+		echo "directory name provided"
+		exit 1
+	elif [ ! -f $file ]
+	then
+		echo "$file does not exist"
+		exit 1
+	fi
+done
+
+echo "end of script"
