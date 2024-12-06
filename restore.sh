681a327fd7c7184baaa811fda60759bc553368cc
kaley-neufeld <120682546+kaley-neufeld@users.noreply.github.com>
2024-12-06 15:54:29 -0500

    restore now moves files; wipes filename from .restore.info

--- restore.sh
+++ restore.sh
@@ -4,6 +4,15 @@ args=("$@")
 recycle_bin="$HOME/deleted"
 yes_array=("y" "Y" "yes")
 
+# restore arg1 filename to arg2 filepath and remove file info from .restore.info
+function restore () {
+	echo "log - in restore function"
+	echo "log - restoring $1 to $2"
+	mv $recycle_bin/$1 $2
+	sed -i '/'$1'/d' .restore.info
+}
+
+
 if [ ${#args} == 0 ]
 then
 	echo "error - no filename provided"
@@ -13,26 +22,35 @@ fi
 for file in ${args[@]}
 do
 	echo "log - processing $file"
+	# check that file to restore exists in recycle bin
 	if [ ! -f $recycle_bin/$file ]
 	then
 		echo "error - $file does not exist"
 		exit 1
 	else
 		read restore_info <<< $(grep -i $file $HOME/.restore.info)
+		read orig_file_name <<< $(cut -d ":" -f1 <<< $restore_info)
 		read orig_file_path <<< $(cut -d ":" -f2 <<< $restore_info)
 		echo "original file path: $orig_file_path"
+
+		# check if file exists in target directory
 		if [ -f $orig_file_path ]
 		then
-			read -p "Do you want to overwrite? y/n " confirm
-			echo "you said: $confirm"
+			read -p "$orig_file_path exists, do you want to overwrite? y/n " confirm
 			if [[ ${yes_array[@]} =~ $confirm ]]
 			then
 				echo "log - overwriting"
+				restore $file $orig_file_path
 			else
 				echo "log - no overwrite, skipping $file"
 			fi
+		else
+			echo "log - now restoring $file"
+			restore $file $orig_file_path
 
 		fi
 		
 	fi
 done
+
+echo "log - end of restore script"
