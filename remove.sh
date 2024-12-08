f37503baf755257942132c7af6b90fa366c0a10e
kaley-neufeld <120682546+kaley-neufeld@users.noreply.github.com>
2024-12-08 15:25:24 -0500

    adding flag functionality; -i operational

--- remove.sh
+++ remove.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-args=("$@")
 recycle_bin="$HOME/deleted"
+i_flag=''
+v_flag=''
+
+while getopts 'iv' flag; do
+	case $flag in
+		i) i_flag='true';;
+		v) v_flag='true';;
+		\?) echo "invalid flag $flag";;
+	esac
+done
+echo "log - flags: $i_flag $v_flag"
+shift $((OPTIND - 1))
+args=("$@")
+
+function remove () {
+	read -a file_info <<< $(ls -i $1)
+	read orig_file_path <<< $(readlink -f $1)
+	new_file_name="${file_info[1]}_${file_info[0]}"
+	echo "log - removed file will be called $new_file_name in recycle bin"
+	tee -a $HOME/.restore.info <<< "$new_file_name:$orig_file_path"
+	mv $1 $recycle_bin/$new_file_name
+}
+
 
 if [ -d "$recycle_bin" ];
 then
@@ -33,13 +55,23 @@ do
 	then	
 		echo "error - attempting to delete remove - operation aborted"
 		exit 1
+	elif [ $(readlink -f file) == $(readlink -f $HOME/project/restore.sh) ]
+	then
+		echo "error - attemping to delete restore - operation aborted"
+		exit 1	
 	else
-		read -a file_info <<< $(ls -i $file)
-		read orig_file_path <<< $(readlink -f $file)
-		new_file_name="${file_info[1]}_${file_info[0]}"
-		echo "log - removed file will be called $new_file_name"
-		tee -a $HOME/.restore.info <<< "$new_file_name:$orig_file_path"
-		mv $file $recycle_bin/$new_file_name
+		if [ $i_flag ]
+		then
+			read -p "remove file $file ? y/n " confirm
+			if [[ ${yes_array[@]} =~ $confirm ]]
+			then 
+				remove $file
+			else
+				echo "skipping $file"
+			fi
+		else
+			remove $file
+		fi
 	fi
 done
 
